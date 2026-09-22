00072ef76311b769c81ef381bcb7ae5fdc52d216
tudoparasuafestape-dot <tudoparasuafestape@gmail.com>
2026-09-22 11:04:15 -0300

    test: corrigir resposta do mock no gate de jornada

--- tests/e2e/navigation-journey.spec.js
+++ tests/e2e/navigation-journey.spec.js
@@ -62,7 +62,7 @@ async function installMock(page,state){
     await route.fulfill({
       status:200,
       contentType:'text/html; charset=utf-8',
-      body:'<!doctype html><html><body><script>window.top.postMessage('+payload+', "*");<\\/script></body></html>'
+      body:'<!doctype html><html><body><script>window.top.postMessage('+payload+', "*");<\/script></body></html>'
     });
   });
 }
